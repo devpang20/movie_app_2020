8720cd6191fad6e1d1dec8199cee3c0fe4505002
sik2 <devpang20@gmail.com>
2020-12-03 22:46:11 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-function Test() {
-  return <h1>Test section</h1>
+function Test({ item }) {
+  return <h1>Test {item} section</h1>
+}
+
+function Test2(props) {
+  console.log(props)
+  return <h1>Test {props.item} section</h1>
 }
 
 function App() {
   return (
     <div className="App">
       Hello World!!!
-      <Test />
+      <Test item='one'/>
+      <Test item='two'/>
+      <Test item='three'/>
+      <Test2
+        a='test2'
+        b='console'
+      />
     </div>
   );
 }
