f7a81de2a122781a44eebfb8162feca1f45bbcf4
sik2 <devpang20@gmail.com>
2020-12-05 14:56:48 +0900

    state life cycle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App  extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor")
+  }
   state = {
     count: 0
   };
@@ -12,7 +16,19 @@ class App  extends React.Component {
   minus = () => {
     this.setState(current => ({ count: current.count -1 }));
   };
+  componentDidMount(){
+    console.log("component rendered")
+  }
+  componentDidUpdate(){
+    console.log("update")
+  }
+
+  componentWillUnmount(){
+    console.log("bye") //retry
+  }
+
   render() {
+    console.log("rendering")
     return (
       <div>
         <h1>Class Component {this.state.count}</h1>
