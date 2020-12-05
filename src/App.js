1eefdccb542296fe53b6aede7c5acf7b213599dd
sik2 <devpang20@gmail.com>
2020-12-05 13:16:32 +0900

    key id add

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import React from 'react';
 
-function Test({ name, image}) {
-  return (
-    <div> 
-      <h2>Test {name} section</h2>
-      <img src={image} />
-    </div>
-  )
-
-}
-
   const itemList = [
     {
+      id: 1,
       name: "item1",
       image: "imageUrl"
     },
     {
+      id: 2,
       name: "item2",
       image: "imageUrl"
     },
     {
+      id: 3,
       name: "item3",
       image: "imageUrl"
     },
     {
+      id: 4,
       name: "item4",
       image:
       "imageUrl"
     },
     {
+      id: 5,
       name: "item5",
       image:
       "imageUrl"
     },
     {
+      id: 6,
       name: "item6",
       image:
       "imageUrl"
     },
     {
+      id: 7,
       name: "item7",
       image:
       "imageUrl"
     }
   ]
+
+  function Test({ name, image}) {
+    return (
+      <div> 
+        <h2>Test {name} section</h2>
+        <img src={image} alt={name} />
+      </div>
+    )
+  }
 
 
 function App() {
@@ -51,7 +57,7 @@ function App() {
     <div className="App">
       <h1>printItem</h1>
       {itemList.map(item => (
-        <Test name={item.name}  image={item.image} />
+        <Test key={item.id} name={item.name}  image={item.image} />
       ))}
     </div>
   );
