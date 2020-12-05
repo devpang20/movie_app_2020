0883188a350b15a3c8ce9178f864fce0cca75313
sik2 <devpang20@gmail.com>
2020-12-05 15:09:30 +0900

    ex componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
 
 class App  extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor")
-  }
   state = {
-    count: 0
+    isLoading : true,
+    movies: []
   };
+  componentDidMount() {
+      setTimeout (() => {
+        this.setState({ isLoading: false });
+      }, 3000)
 
-  plus = () => {
-    this.setState(current => ({ count: current.count +1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
-  };
-  componentDidMount(){
-    console.log("component rendered")
   }
-  componentDidUpdate(){
-    console.log("update")
-  }
-
-  componentWillUnmount(){
-    console.log("bye") //retry
-  }
-
   render() {
-    console.log("rendering")
-    return (
-      <div>
-        <h1>Class Component {this.state.count}</h1>
-        <button onClick={this.plus}>+</button>
-        <button onClick={this.minus}>-</button>
-      </div>
-    )
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "Ready"}</div>;
+
   }
 }
 
