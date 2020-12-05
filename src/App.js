3c326dc84f0e6e021c62fe7c81cb4b27fa61bdb3
sik2 <devpang20@gmail.com>
2020-12-05 14:17:45 +0900

    class components and state

--- src/App.js
+++ src/App.js
@@ -1,80 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-  const itemList = [
-    {
-      id: 1,
-      num: 3,
-      name: "item1",
-      image: "imageUrl"
-    },
-    {
-      id: 2,
-      num: 3,
-      name: "item2",
-      image: "imageUrl"
-    },
-    {
-      id: 3,
-      num: 3,
-      name: "item3",
-      image: "imageUrl"
-    },
-    {
-      id: 4,
-      num: 3,
-      name: "item4",
-      image:
-      "imageUrl"
-    },
-    {
-      id: 5,
-      num: 3,
-      name: "item5",
-      image:
-      "imageUrl"
-    },
-    {
-      id: 6,
-      num: 3,
-      name: "item6",
-      image:
-      "imageUrl"
-    },
-    {
-      id: 7,
-      num: 3,
-      name: "item7",
-      image:
-      "imageUrl"
-    }
-  ]
+class App  extends React.Component {
+  state = {
+    count: 0
+  };
 
-  function Test({ name, image, num}) {
+  plus = () => {
+    console.log("plus")
+  };
+  minus = () => {
+    console.log("minus")
+  };
+  render() {
     return (
-      <div> 
-        <h2>Test {name} section</h2>
-        <h4>{num}</h4>
-        <img src={image} alt={name} />
+      <div>
+        <h1>Class Component {this.state.count}</h1>
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.minus}>-</button>
       </div>
     )
   }
-
-  Test.propTypes = {
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    num: PropTypes.number.isRequired,
-  }
-
-function App() {
-  return (
-    <div className="App">
-      <h1>printItem</h1>
-      {itemList.map(item => (
-        <Test key={item.id} name={item.name}  image={item.image} num= {item.num} />
-      ))}
-    </div>
-  );
 }
 
 export default App;
