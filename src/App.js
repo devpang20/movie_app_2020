9adc824ecd33f41e6206c60e91ccbeb650c12979
sik2 <devpang20@gmail.com>
2020-12-08 00:05:47 +0900

    feedBack

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import Movie from './Movie'
+import './App.css'
 
-class App  extends React.Component {
+class App extends React.Component {
   state = {
-    isLoading : true,
+    isLoading: true,
     movies: []
-  };
+  }
+
   getMovie = async () => {
     const { 
         data: {
           data: { movies }
         }
-      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+      } = await axios.get('https://yts-proxy.now.sh/list_movies.json', {
+        sort_by: 'rating'
+      })
+
     this.setState({ movies, isLoading: false })
-  };
+  }
 
   componentDidMount() {
-    this.getMovie();
+    this.getMovie()
   }
+  
   render() {
     const { isLoading, movies } = this.state;
+    const Movies = movies.map(movie => (
+        <Movie   
+          key={movie.id}
+          id={movie.id}   
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image} 
+          genres={movie.genres} 
+        />
+    ))
+
     return (
       <section className="container">
           {isLoading 
-            ? 
+            ?
               <div className="loader">
                 <span className="loader__text">Loding Data</span>
               </div>
             :
-              <div className="movies">
-                {movies.map(movie => (
-                    <Movie   
-                      key={movie.id}
-                      id={movie.id}   
-                      year={movie.year} 
-                      title={movie.title} 
-                      summary={movie.summary} 
-                      poster={movie.medium_cover_image} 
-                      genres={movie.genres} 
-                    />
-                ))}
-              </div>
+              <div className="movies">{Movies}</div>
           }   
       </section>
     )
@@ -50,4 +55,4 @@ class App  extends React.Component {
   }
 }
 
-export default App;
+export default App
